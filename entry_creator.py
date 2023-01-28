3497492a62bf852991d433d861b9a5838176d34c
jakobnunnendorf <95524074+jakobnunnendorf@users.noreply.github.com>
2023-01-28 17:57:46 +0800

    Working final version

--- entry_creator.py
+++ entry_creator.py
@@ -1,20 +1,16 @@
-def create_entry(entry_title, entry_date, entry_time, entry_path):
+def create_entry(entry_title, entry_content, entry_path):
     # create template according to this format:
     #	Date:	28 January 2023 at 1:48:56 PM SGT
 	#   Location:	Yale NUS College, Singapore
 
     # Example Header
     # Lorem ipsum
-    first_line = "    Date:    " + entry_date + " at " + entry_time + "\n"
-    second_line = "    Location:	" + "\n" + "\n"
-    body = entry_title + "\n\nTLDR:\n\nResults:\n\nComments:\n\n"
-    entry_template = first_line + second_line + body
     
     # create the txt file:
     text_file = open(entry_path+"/"+entry_title+".txt", "w")
 
     #write string to file
-    text_file.write(entry_template)
+    text_file.write(entry_content)
     
     #close file
     text_file.close()
